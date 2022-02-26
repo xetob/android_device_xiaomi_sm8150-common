3db6e847919fe30155584eacec5a52ead5c108ee
Nauval Rizky <enuma.alrizky@gmail.com>
2022-02-27 00:54:46 +0700

    sm8150-common: Move msm_irqbalance config to device tree
    Change-Id: I5210e90a06f8af6706cd222b703c1ff7f7acc3f5

--- msmnile.mk
+++ msmnile.mk
@@ -226,6 +226,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# IRQbalance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
