7c1791c628bd2833ef35ddacdcfece81f63ba2b9
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-05-20 00:29:06 +0200

    sm8150-common: Build tinymix
    Change-Id: Ide56569ff42f0d576d3237fa37a0e8d51bed6784

--- msmnile.mk
+++ msmnile.mk
@@ -119,7 +119,8 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot \
     libvisualizer \
-    libvolumelistener
+    libvolumelistener \
+    tinymix
 
 # Audio configs
 PRODUCT_COPY_FILES += \
