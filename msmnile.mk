4b01dd4f7b1fe176acaaaae487f97edd6dd8358e
chiteroman <querveros999@gmail.com>
2024-08-09 00:29:35 +0200

    sm8150-common: declare hardware keystore feature
    Change-Id: If435759fa915bc7efe8e2c3d2383b44c1feb69c0

--- msmnile.mk
+++ msmnile.mk
@@ -43,7 +43,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
+    hardware/interfaces/keymaster/4.1/default/android.hardware.hardware_keystore.km41.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hardware_keystore.km41.xml
 
 ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
 PRODUCT_COPY_FILES += \
