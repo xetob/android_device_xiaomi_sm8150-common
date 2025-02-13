5dd4aa32371ba782a8dd02411c251c207de666cb
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 15:40:58 +0800

    sm8150-common: Disable UFFD GC via `OVERRIDE_ENABLE_UFFD_GC`
    * Our kernel (4.14) does not support features needed for UFFD GC.
      Leaving it enabled results in long waiting time on the first boot.
    
    Change-Id: Ia8a17467e13d343c03ee29e514994946e7e84985

--- msmnile.mk
+++ msmnile.mk
@@ -352,6 +352,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# UFFD GC
+OVERRIDE_ENABLE_UFFD_GC := false
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
