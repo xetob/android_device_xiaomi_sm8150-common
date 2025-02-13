c1f78ea04aaf42436f70f792b7741539c9a18f8d
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 14:48:54 +0800

    sm8150-common: Build QTI Thermal AIDL HAL
    Change-Id: I21406af9d90fe6be1402f30e1b19958e4f46eec9

--- msmnile.mk
+++ msmnile.mk
@@ -352,6 +352,10 @@ PRODUCT_BOOT_JARS += \
     telephony-ext \
     xiaomi-telephony-stub
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.qti
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
