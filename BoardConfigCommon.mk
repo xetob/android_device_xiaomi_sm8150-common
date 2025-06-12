d954a35db62cc56095c4d5b132bf359ae7127c98
armdebug <armdebug@protonmail.com>
2025-06-12 10:54:01 -0400

    sm8150-common: Update blobs from OS1.0.6.0.TKXMIXM
    Change-Id: Id3c51f0e7a0bea7a8db6ca6176b578f924e2770b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,7 +199,7 @@ SOONG_CONFIG_XIAOMI_MSMNILE_PARTITION_SCHEME := legacy
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-03-01
+VENDOR_SECURITY_PATCH := 2024-10-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
