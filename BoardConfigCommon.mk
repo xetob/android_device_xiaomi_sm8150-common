d9ff285969d6c419c9358bd273cc8ab053c16e5f
basamaryan <basam.aryan@gmail.com>
2026-01-26 02:12:42 -0800

    sm8150-common: Bump kernel BPF version override to 5.10.239
    This matches android12-5.10
    
    Test: atest netd_integration_test
    Change-Id: I5e528f630ee47d396bee8dac86938f64992406d6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,6 +155,7 @@ TARGET_BOARD_PLATFORM := msmnile
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 ifneq ($(TARGET_IS_TABLET),true)
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor_phone.prop
